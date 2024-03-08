6ffb102fafcf376e9bfa7ca835892e6122ec598e
JajorNitrodolski <sonic579x@gmail.com>
2024-03-08 03:37:18 +0100

    komit 2 zadanie gut

--- 2.py
+++ 2.py
@@ -1,3 +1,11 @@
 import sys
 
+sys.stdout.write("Podaj 'a': ")
 a=int(sys.stdin.readline())
+sys.stdout.write("Podaj 'b': ")
+b=int(sys.stdin.readline())
+sys.stdout.write("Podaj 'c': ")
+c=int(sys.stdin.readline())
+d=str(a**b+c)
+sys.stdout.write(d)
+
